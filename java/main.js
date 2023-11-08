05c1e5cc1aa7aba3293b997bd80dcea366b55149
kristiafiliposki <filiposki.kristian1112@gmail.com>
2023-11-08 13:08:34 +0100

    first commit

---
+++ java/main.js
@@ -0,0 +1,124 @@
+let boxes;
+
+/* clicco la modalita */
+
+/* facile */
+document.getElementById("facile").addEventListener("click",function(){
+    
+    console.log(boxes)
+    boxes=parseInt(100);
+    let n = 0
+
+})
+
+/* medio */
+document.getElementById("medio").addEventListener("click",function(){
+    
+    let n = 0
+    boxes=parseInt(81);
+    console.log(`ciao le caselle sono ${boxes}`)
+    
+
+})
+
+/* difficile */
+document.getElementById("difficile").addEventListener("click",function(){
+    
+    boxes=parseInt(49);
+    console.log(boxes)
+    
+    let n = 0    
+})
+
+
+/* play */
+document.getElementById("play").addEventListener("click",function(){
+    
+    console.log(boxes)
+
+    if(boxes=100){
+
+        const container=document.getElementById("container");
+        let box=document.querySelector("#container div")
+        let newDiv = document.createElement("div");
+    
+    
+        while(  n < boxes) {
+            const numbers=n;
+            n++;
+            console.log(n);
+            GenBox(n)
+        }
+        
+        function GenBox(){
+            let newDiv = document.createElement("div");
+            newDiv.innerHTML=(n)
+            container.append(newDiv);
+            newDiv.classList.add("box-facile");
+            newDiv.addEventListener("click",function(){
+            newDiv.classList.toggle('highlight');
+            })
+        }   
+
+    
+    }
+
+    else if(boxes=81){
+        const container=document.getElementById("container");
+        const boxes=81;
+    
+    
+        let box=document.querySelector("#container div")
+        let newDiv = document.createElement("div");
+    
+    
+        while(  n < boxes) {
+            const numbers=n;
+            n++;
+            console.log(n);
+            GenBox(n)
+        }
+
+        function GenBox(){
+            let newDiv = document.createElement("div");
+            newDiv.innerHTML=(n)
+            container.append(newDiv);
+            newDiv.classList.add("box-medio");
+            newDiv.addEventListener("click",function(){
+            newDiv.classList.toggle('highlight');
+            })
+        }   
+
+    }
+
+    else if(boxes=49){
+        const container=document.getElementById("container");
+        const boxes=49;
+    
+    
+        let box=document.querySelector("#container div")
+        let newDiv = document.createElement("div");
+    
+    
+        while(  n < boxes) {
+            const numbers=n;
+            n++;
+            console.log(n);
+            GenBox(n)
+        }
+
+        function GenBox(){
+            let newDiv = document.createElement("div");
+            newDiv.innerHTML=(n)
+            container.append(newDiv);
+            newDiv.classList.add("box-difficile");
+            newDiv.addEventListener("click",function(){
+            newDiv.classList.toggle('highlight');
+            })
+        }   
+
+    }
+    
+})
+
+
